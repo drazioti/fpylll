1b1850f891b60dfb3de29fb772b8551c2ba13ae8
drazioti <drazioti@gmail.com>
2020-07-21 18:09:19 +0300

    jupyterhub installation was added

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,10 @@
 
 # Create Virtual Environment
 
-rm -rf fpylll-env
-virtualenv fpylll-env
-cat <<EOF >>fpylll-env/bin/activate
+rm -rf fpylll-env-py2
+#virtualenv fpylll-env
+virtualenv -p  /usr/bin/python2.7 fpylll-env-py2 # for python2
+cat <<EOF >>fpylll-env-py2/bin/activate
 ### LD_LIBRARY_HACK
 _OLD_LD_LIBRARY_PATH="\$LD_LIBRARY_PATH"
 LD_LIBRARY_PATH="\$VIRTUAL_ENV/lib:\$LD_LIBRARY_PATH"
@@ -23,13 +24,14 @@ export CFLAGS
 export CXXFLAGS
 EOF
 
-ln -s fpylll-env/bin/activate
+ln -s fpylll-env-py2/bin/activate
 
 source activate
-
 pip install -U pip
 
 # Install FPLLL
+
+#sudo apt-get install python2-dev # if you get an error in cysignals execute this command
 
 git clone https://github.com/fplll/fplll fpylll-fplll
 cd fpylll-fplll || exit
@@ -44,6 +46,7 @@ cd ..
 pip install Cython
 pip install -r requirements.txt
 pip install -r suggestions.txt
+pip3 install -r suggestions3.txt
 python setup.py clean
 python setup.py build_ext
 python setup.py install
@@ -51,3 +54,6 @@ python setup.py install
 
 echo "Don't forget to activate environment each time:"
 echo " source ./activate"
+echo "For instanlling jupyterhub:"
+echo "pip3 install jupyterhub"
+echo "npm install configurable-http-proxy"
